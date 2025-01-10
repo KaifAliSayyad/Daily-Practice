e2787e0ee75b0af61f42b42d4733a48559b654cc
KaifAliSayyad <kaifali.sayyad@wissen.com>
2025-01-10 16:52:26 +0530

    daily_practice_10-01-2025

---
+++ 10-01-2025/AbstractDemo.java
@@ -0,0 +1,24 @@
+abstract class A{
+	// abstract int x = 10;
+	A(){}
+	public abstract void abc();
+	public void xyz(){
+
+	}
+}
+
+
+abstract class B extends A{
+
+}
+
+class C extends B{
+	public void abc(){
+
+	}
+}
+public class AbstractDemo{
+	public static void main(String[] args){
+		//B b1 = new B();	
+	}
+}
---
+++ 10-01-2025/StaticDemo.java
@@ -0,0 +1,30 @@
+class A{
+	static int x;
+	static int y = 20;
+	public static void abc(){
+		System.out.println("From static method : "+y);
+		xyz();
+	}
+
+	public static void xyz(){
+		System.out.println("From xyz() method");
+	}
+}
+
+public class StaticDemo{
+	static{
+		System.out.println("1. From static block...");
+	}
+
+	public static void main(String[] args){
+		A a1 = new A();
+		A a2 = new A();
+		A a3 = new A();
+		a1.x = 10;
+		a2.x = 20;
+		a3.x = 30;
+
+		a2.x = 50;
+		System.out.println(A.x);	
+	}
+}
